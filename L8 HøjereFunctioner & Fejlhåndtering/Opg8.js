9c302b463d116e57c4db8909fa74aef14fe95961
TAULO <taulo@live.dk>
2021-09-20 13:40:23 +0200

    Løst opgave halvdelen af 8.1

---
+++ L8 HøjereFunctioner & Fejlhåndtering/Opg8.js
@@ -0,0 +1,37 @@
+// opgave 8.1
+let personer = [
+    {
+        navn : "Casper", 
+        alder : 20,
+        mobilnummer : "2244556677"
+    },
+    {
+        navn : "Oliver", 
+        alder : 35,
+        mobilnummer : "8899001122"
+    },
+    {
+        navn : "Tobias", 
+        alder : 25,
+        mobilnummer : "7766882200"
+    },
+    {
+        navn : "Rasmus", 
+        alder : 15, 
+        mobilnummer : "1721927182"
+    }
+];
+
+console.log(personer)
+
+// find person med bestemt telefon nr
+let findTelefonNr = personer.find((person) => person.mobilnummer === "2244556677");
+console.log("Bestemt telefonnr: " + findTelefonNr.navn);
+
+// find den mindste alder 
+let findMindsteAlder = personer.reduce((prev, curr) => prev.alder < curr.alder ? prev : curr)
+console.log("Mindste alder: " + findMindsteAlder.navn);
+
+// giv hvert object person en fortløbende ID 
+let givID = personer.forEach((element, id = 0) => element.id = id + 1);
+console.log(personer);
