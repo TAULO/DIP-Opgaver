35ad3d59455d2699e7d2dcd11e9376048a59b781
Thomas <taulo@live.dk>
2021-09-29 21:25:40 +0200

    Ændrede navnet på L8. Færdiggjort L9.2 alle metoder i Stack

--- L9 Prototyper, Constructor funktioner & Class notation/Opg9.2.js
+++ L9 Prototyper, Constructor funktioner & Class notation/Opg9.2.js
@@ -4,15 +4,39 @@ class StringStack {
       this.size = -1;
       this.stackArr = [];
     }
+    peek() { return this.stackArr[this.size] }
+
     push(e) {
         this.size++;
         this.stackArr[this.size] = e;    
     }
+
     pop() {
-        this.stackArr[this.size] = null;
-        this.size--;
+        if (this.size > -1) {
+            this.stackArr[this.size] = null;
+            this.size--;
+        } 
     }
-    getStack() { return this.stackArr; }
+
+    isEmpty() { return this.size === -1 }
+
+    clear() {
+        while(!this.isEmpty()) this.pop();
+    }
+
+    contains(e) {
+        for (let i = this.size; i >= -1; i--) {
+            if (e === this.stackArr[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    search(e) { return this.stackArr.indexOf(e) }
+
+    getStack() { return this.stackArr }
+    
     getSize() { return this.size + 1 }
 }
 
@@ -22,5 +46,21 @@ stack.push("World");
 
 console.log(stack.getStack());
 stack.pop();
+stack.pop();
 console.log(stack.getStack());
 console.log(stack.getSize());
+stack.push("World");
+stack.push("World");
+console.log(stack.getStack());
+console.log(stack.getSize());
+console.log(stack.isEmpty())
+stack.clear();
+console.log(stack.isEmpty())
+console.log(stack.getSize());
+stack.push("Hello");
+stack.push("Friend");
+stack.push("Good");
+console.log(stack.getStack());
+console.log(stack.getSize());
+console.log(stack.contains("Hello"));
+console.log(stack.search("Good"))
