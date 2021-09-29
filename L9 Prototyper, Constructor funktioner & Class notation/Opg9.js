a80cc3ff5d2a62bedd98025ace7898147d2bfb6c
TAULO <taulo@live.dk>
2021-09-29 11:56:39 +0200

    Startede på opgave 9, samt ændret navnet på nogle af mapperne

---
+++ L9 Prototyper, Constructor funktioner & Class notation/Opg9.js
@@ -0,0 +1,42 @@
+// opgave 9.1 
+class Person {
+    constructor(navn) {
+        this.navn = navn;
+    }
+    toString() { return this.navn }
+    equals(p) { return p.navn === this.navn && p.constructor === Person }    
+    compare(p1, p2) { return p1.navn > p2.navn }
+}
+
+class Studerende extends Person {
+    constructor(navn, id) {
+        super(navn);
+        this.id = id;
+    }
+    equals(s) { return s.navn === this.navn && s.id === this.id && s.constructor === Studerende }
+}
+
+let person1 = new Person("Simon");
+let person2 = new Person("Simon");
+let person3 = new Person("Josefine");
+let person4 = new Person("Caroline");
+
+let studerende1 = new Studerende("Casper", 1);
+let studerende2 = new Studerende("Casper", 1);
+let studerende3 = new Studerende("Fin", 2);
+let studerende4 = new Studerende("Sigurd", 3);
+
+
+// tjekker om to personer er det samme
+console.log(person1.equals(person2));
+
+// sammenligner to personer
+console.log(person1.compare(person2, person3));
+
+// tjekker om to studerende er det samme
+console.log(studerende1.equals(studerende2));
+
+// sammenligner to studerende 
+console.log();
+
+let personer = [person1, person2, person3, person4, studerende1, studerende2, studerende3, studerende4];
